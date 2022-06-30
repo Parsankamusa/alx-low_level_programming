c0a5fdf4530a5bfce33444f13f16378254a3fd68
Parsankamusa <parsankamusa@gmail.com>
2022-06-30 11:41:12 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   * Description: 'the program's description'
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
+ * Return: Always 0 (Success)
+ */
+int main (void)
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like buildng a multilingual puzzle");
+	return (0);
 }
