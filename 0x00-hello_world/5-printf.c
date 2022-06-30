05c242b6293564b4e5dd8ae4854160ae053ecd37
Parsankamusa <parsankamusa@gmail.com>
2022-06-30 12:16:36 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
+
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   * Description: 'C program for print'
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	return (0);
 }
