4975eb1d3ad6d3f9a8d8885c896a4d0fc923f4b3
Parsankamusa <parsankamusa@gmail.com>
2022-07-05 06:08:02 -0700

    a function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,6 +2,7 @@
 
 /**
  * _isalpha - Check if character is an alphabetic character.
+ * @c: type int character
  * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
  */
 int _isalpha(int c)
