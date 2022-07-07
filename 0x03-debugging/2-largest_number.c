053d6a1826a549685f54ed1895ddd1d5e4a02d50
Parsankamusa <parsankamusa@gmail.com>
2022-07-07 08:20:57 -0700

    This program prints the largest of three integers.

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+
 /**
  * * largest_number - returns the largest of 3 numbers
  *  @a: first integer
@@ -6,6 +8,7 @@
  *  @c: third integer
  *  Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -14,13 +17,16 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
+
 	else if (b >= a && b >= c)else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
+
 	else
 	{
 		largest = c;
 	}
-	return (largest);
+
+	return (largest);	
 }
