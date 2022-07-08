97c2c0e2bcf2c20539bd27bf27402510664cf690
Parsankamusa <parsankamusa@gmail.com>
2022-07-08 11:43:25 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "main.h"
 /**
  * main - entry point
@@ -19,3 +20,26 @@ int main(void)
 
 	return (0);
 }
+=======
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+>>>>>>> 8e54e903139f9c7fe1238bc60252aae4096daaed
