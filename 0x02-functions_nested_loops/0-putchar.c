b7ff31a38bcaf5da12765664f00cf3af22c75849
Parsankamusa <parsankamusa@gmail.com>
2022-07-08 12:05:56 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include "main.h"
 /**
- * main - entry point
+ * main - prints putchar
  *
- * Return: always 0
+ *  Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -23,41 +18,3 @@ int main(void)
 
 	return (0);
 }
-=======
-=======
->>>>>>> 8e54e903139f9c7fe1238bc60252aae4096daaed
-=======
->>>>>>> 8e54e903139f9c7fe1238bc60252aae4096daaed
-=======
->>>>>>> 8e54e903139f9c7fe1238bc60252aae4096daaed
-#include "main.h"
-/**
- * main - entry point
- *
- * Return: always 0
- */
-
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
-}
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 8e54e903139f9c7fe1238bc60252aae4096daaed
-=======
->>>>>>> 8e54e903139f9c7fe1238bc60252aae4096daaed
-=======
->>>>>>> 8e54e903139f9c7fe1238bc60252aae4096daaed
-=======
->>>>>>> 8e54e903139f9c7fe1238bc60252aae4096daaed
