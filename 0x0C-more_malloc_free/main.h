b9edc069dcc4ee6cf09dad8cc36022048ff0ce26
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2022-07-28 17:35:04 +0300

    done task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,13 @@
-ifndef FILE_MAIN
-#define FINE_MAIN
-
-void *malloc_checked(unsigned int b);
+#include <stdlib.h>
 int _putchar(char c);
+void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-#endif
+int _strlen(char *s);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
