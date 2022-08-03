95d86b8a1c0f9bda84c5d145076945f17b6a8ded
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2022-08-03 22:16:28 +0300

    done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-/**
+
+/*
  * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
  */
 
 #include <stdlib.h>
