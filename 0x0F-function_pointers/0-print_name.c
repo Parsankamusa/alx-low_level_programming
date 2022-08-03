61af69845532ba4db76f40c1fce3660add212484
Parsankamusa <parsankamusa@gmail.com>
2022-08-03 12:06:58 -0700

    completed task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name - name character
+ * @f - function pointer
+ * Return: No return
+ */
+void print_name(char *name, void (*f)(char *));
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,6 @@
+0x0F. C - Function pointers
+
+/**
+
+A useful technique is the ability to have pointers to functions. Their declaration is easy: write the declaration as it would be for the function, say
+*/
