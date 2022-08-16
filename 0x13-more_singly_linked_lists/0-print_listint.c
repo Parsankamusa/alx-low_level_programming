63cd465c25fb54018a985b24c87a5fa4d64afabd
Parsankamusa <parsankamusa@gmail.com>
2022-08-16 04:57:03 -0700

    completed task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,17 @@
 #include "lists.h"
+
 /**
  * print_listint - numbers of nodes
  * @h: head of nodes
  *
  * Return: numbers of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h != 0)
+	while (h != NULL)
 	{
 		printf("%d \n", h->n);
 		h = h->next;
