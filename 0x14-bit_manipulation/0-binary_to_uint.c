659187bb15302408c2a194df837749cf6d8489a6
Parsankamusa <parsankamusa@gmail.com>
2022-08-18 16:02:11 -0700

    function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - conversion of binary to int
+ * @b: binary
+ *
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0;
+	int len, k;
+
+	if (!b)
+		return (0);
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, k = 1; len >= 0; len--  k *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		if (b[len] & 1)
+		{
+			i += k;
+		}
+	}
+
+	return (i);
+}
