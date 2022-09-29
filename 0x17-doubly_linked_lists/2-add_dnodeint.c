3911de7a4d333f98a4f502994f0d7ccf2b749c0b
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2022-09-30 01:15:21 +0300

    adds a new node at the beginning of a  list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,18 +10,18 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new_node;
 
 	if (head == NULL)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (new_node == NULL)
 		return (NULL);
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-	*head = new;
-	if (new->next != NULL)
-		(new->next)->prev = new;
-	return (new);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	*head_node = new_node;
+	if (new_node->next != NULL)
+		(new_node->next)->prev = new_node;
+	return (new_node);
 }
